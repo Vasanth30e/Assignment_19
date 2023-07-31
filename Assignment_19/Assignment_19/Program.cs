85c37302431778a3983230ac19f42a73634d2b23
unknown <vasanthgamer2000@gmail.com>
2023-07-31 15:25:01 +0530

    Delegates Assignment

---
+++ Assignment_19/Assignment_19/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_19
+{
+    public delegate int ArithmeticOperation(int num1, int num2);
+    internal class Program
+    {
+       
+        public static int Addition(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+        static void Main(string[] args)
+        {
+            ArithmeticOperation add = new ArithmeticOperation(Addition);
+            ArithmeticOperation sub = new ArithmeticOperation(Subtract);
+            ArithmeticOperation multi = new ArithmeticOperation(Multiply);
+            ArithmeticOperation divide = new ArithmeticOperation(Divide);
+            
+            while (true)
+            {
+                Console.WriteLine("Choose an Arithmetic Operation\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division");
+                int operation = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter first number");
+                int number1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter second number");
+                int number2 = int.Parse(Console.ReadLine());
+                switch (operation)
+                {
+                    case 1:
+                        Console.WriteLine($"Addition of {number1} and {number2} is {add(number1, number2)}");
+                        break;
+                    case 2:
+                        Console.WriteLine($"Subtraction of {number1} and {number2} is {sub(number1, number2)}");
+                        break;
+                    case 3:
+                        Console.WriteLine($"Multiplication of {number1} and {number2} is {multi(number1, number2)}");
+                        break;
+                    case 4:
+                        Console.WriteLine($"Division of {number1} and {number2} is {divide(number1, number2)}");
+                        break;
+                    default:
+                        Console.WriteLine("Wrong Choice");
+                        break;
+                }
+                Console.ReadKey();
+            }
+ 
+        }
+    }
+}
---
+++ Assignment_19_Part2/Assignment_19_Part2/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_19_Part2
+{
+    delegate T Operation<T>(T num1, T num2);
+    internal class Program
+    {
+        static T Add<T>(T num1, T num2)
+        {
+            dynamic a = num1;
+            dynamic b = num2;
+            return a + b;
+        }
+
+        static T Subtract<T>(T num1, T num2)
+        {
+            dynamic a = num1;
+            dynamic b = num2;
+            return a - b;
+        }
+
+        static T Multiply<T>(T num1, T num2)
+        {
+            dynamic a = num1;
+            dynamic b = num2;
+            return a * b;
+        }
+
+        static T Divide<T> (T num1, T num2)
+        {
+            dynamic a = num1;
+            dynamic b = num2;
+            return a / b;
+
+        }
+
+        static void Main(string[] args)
+        {
+            Operation<int> addOperation = new Operation<int>(Add);
+            Operation<int> subOperation = Subtract;
+            Operation<int> multiplyOperation = Multiply;
+            Operation<double> divideOperation = Divide;
+
+            while (true)
+            {
+                Console.WriteLine("Choose an Arithmetic Operation\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division");
+                int operation = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter first number");
+                string number1 = Console.ReadLine();
+                int value1;
+                if(!int.TryParse(number1, out value1))
+                {
+                    Console.WriteLine("Invalid Input");
+                    Console.ReadKey();
+                    return;
+                }
+
+                Console.WriteLine("Enter second number");               
+                string number2 = Console.ReadLine();
+                int value2;
+                if (!int.TryParse(number2, out value2))
+                {
+                    Console.WriteLine("Invalid Input");
+                    Console.ReadKey();
+                    return;
+                }
+
+                switch (operation)
+                {
+                    case 1:
+
+                        Console.WriteLine($"Addition of {number1} and {number2} is {addOperation(value1, value2)}");
+                        break;
+                    case 2:
+                        Console.WriteLine($"Subtraction of {number1} and {number2} is {subOperation(value1, value2)}");
+                        break;
+                    case 3:
+                        Console.WriteLine($"Multiplication of {number1} and {number2} is {multiplyOperation(value1, value2)}");
+                        break;
+                    case 4:
+                        Console.WriteLine($"Division of {number1} and {number2} is {divideOperation(value1, value2)}");
+                        break;
+                    default:
+                        Console.WriteLine("Wrong Choice");
+                        break;
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
